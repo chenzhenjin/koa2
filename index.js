b9fa7f1699b3819489a88b6033d3fe54d8874c30
chenzhenjin <971561770@qq.com>
2020-12-04 15:54:10 +0800

    feat(router): 解决京店宝api设置header、证书等条件时，koa2接入时，前端报错

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-const Koa = require("koa")
-const app = new Koa()
-const path = require("path")
-const static = require("koa-static")
-const bodyParse = require("koa-bodyparser")
-const router = require("./router")
-const cross = require("koa2-cors")
+/*
+ * @Author: chenzhenjin
+ * @email: BrotherStudy@163.com
+ * @Date: 2020-09-23 17:26:03
+ * @LastEditTime: 2020-10-23 17:36:50
+ * @Descripttion: 模块描述
+ */
+const Koa = require("koa");
+const app = new Koa();
+const path = require("path");
+const static = require("koa-static");
+const bodyParse = require("koa-bodyparser");
+const router = require("./router");
+const cross = require("koa2-cors");
 
+app.use(
+  cross({
+    exposeHeaders: ["WWW-Authenticate", "Server-Authorization", "Date"],
+    maxAge: 100,
+    credentials: true,
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Accept",
+      "X-Custom-Header",
+      "anonymous",
+      "os-type",
+      "securitycode",
+      "securityKey",
+      "token"
+    ]
+  })
+);
+app.use(bodyParse());
+let staticPath = "./page";
+app.use(static(path.join(__dirname, staticPath)));
 
-app.use(cross())
-app.use(bodyParse())
-let staticPath = './page'
-app.use(static(path.join(__dirname,staticPath)))
+app.use(router.routes()).use(router.allowedMethods());
 
-app.use(router.routes()).use(router.allowedMethods())
-
-app.listen(3000,()=>{
-  console.log("back-end running")
-}) 
+app.listen(3000, () => {
+  console.log("back-end running");
+});
--- router/index.js
+++ router/index.js
@@ -1,239 +1,103 @@
-const Router = require("koa-router")
-let reactMovie = new Router()
-let vueShop = new Router()
-let router = new Router()
-const { findAll, insertOne, cleanAll } = require("../server/history")
-const { newsFindAll, newsFindOne, commentFindAll, commentInsert, articleInsert } = require("../server/news")
-const { shopInsert, shopsFindAll, shopsFindOne } = require("../server/shop")
-const { carInsert, carFindShopAll, carFindAll, carUpdate, carDelete, carInsertOne } = require("../server/car")
-const { movieInsert, movieTypeFind,movieRegexFind} = require("../server/movie")
-const mongoose = require("mongoose")
-const axios = require("axios")
-function swichReactDB(dbName) {
-  if (swichReactDB.react) {
-    return new Promise((resolve, reject) => { resolve(true) })
-  }
-  return new Promise((resolve, reject) => {
-    mongoose.connect("mongodb://localhost/" + dbName, err => {
-      if (err) throw err
-      console.log("react connect")
-      swichReactDB.react = true
-      swichVueDB.vue = false
-      resolve(true)
-    })
+/*
+ * @Author: chenzhenjin
+ * @email: BrotherStudy@163.com
+ * @Date: 2020-09-23 17:26:03
+ * @LastEditTime: 2020-10-23 18:55:02
+ * @Descripttion: 模块描述
+ */
+const Router = require("koa-router");
+let reactMovie = new Router();
+let uniapp = new Router();
+let router = new Router();
+let trq = new Router();
+const axios = require("axios");
+reactMovie
+  .get("history", async (ctx) => {
+    let connect = await swichReactDB(ctx.url.split("/")[1]);
+    let data = await findAll();
+    console.log("findAll get", data);
+    ctx.body = data;
   })
-}
-function swichVueDB(dbName) {
-  if (swichVueDB.vue) {
-    return new Promise((resolve, reject) => { resolve(true) })
-  }
-  return new Promise((resolve, reject) => {
-    mongoose.connect("mongodb://localhost/" + dbName, err => {
-      if (err) throw err
-      console.log("vue connect")
-      swichReactDB.react = false
-      swichVueDB.vue = true
-      resolve(true)
-    })
+  .post("history/add", async (ctx) => {
+    let connect = await swichReactDB(ctx.url.split("/")[1]);
+    let data = ctx.request.body;
+    let res = await insertOne(data);
+    ctx.body = res;
   })
-}
-function moivesAxios(url, methods) {
-  return new Promise((resolve, reject) => {
-    let apikey = "0df993c66c0c636e29ecbb5344252a4a"
-    if (methods === 'get') {
-      axios.get(`http://api.douban.com/v2/${url}?count=100&apikey=${apikey}`).then(res => {
-        let filterData = {}
-        filterData.total = res.data.total
-        filterData.subjects = res.data.subjects.map(item => {
-          let obj = {}
-          obj.images = {small:""}
-          obj.images.small = item.images.small
-          obj.title = item.title
-          obj.genres = item.genres
-          obj.rating = {average:0}
-          obj.rating.average = item.rating.average
-          obj.id = item.id
-          obj.type = url.split('/')[1]
-          return obj
-        })
-        resolve(filterData) 
-      })
+  .post("history/clean", async (ctx) => {
+    let connect = await swichReactDB(ctx.url.split("/")[1]);
+    let res = await cleanAll();
+    ctx.body = res;
+  })
+  .get("movie/:type", async (ctx) => {
+    let connect = await swichReactDB(ctx.url.split("/")[1]);
+    let query = { count: +ctx.query.count, start: +ctx.query.start };
+    let type = ctx.params.type;
+    let res = {};
+    if (type === "search_result") {
+      res = await movieRegexFind(ctx.query.search, query);
+    } else {
+      res = await movieTypeFind(type, query);
     }
+    // let url = ctx.url.split('/')[2] + '/' + ctx.url.split('/')[3]
+    // let data = await moivesAxios(url, "get")
+    // console.log("movie/in_theaters", data)
+    // let res = await movieInsert(data.subjects)
+    ctx.body = res;
+  });
+
+uniapp
+  .get("list", async (ctx) => {
+    console.log("uniapp query", JSON.stringify(ctx.query, null, 4));
+    ctx.body = [{ home: "123", about: "456" }];
   })
-}
-reactMovie.get('history', async (ctx) => {
-  let connect = await swichReactDB(ctx.url.split('/')[1])
-  let data = await findAll()
-  console.log("findAll get", data)
-  ctx.body = data
-}).post('history/add', async (ctx) => {
-  let connect = await swichReactDB(ctx.url.split('/')[1])
-  let data = ctx.request.body
-  let res = await insertOne(data)
-  ctx.body = res
-}).post("history/clean", async (ctx) => {
-  let connect = await swichReactDB(ctx.url.split('/')[1])
-  let res = await cleanAll()
-  ctx.body = res
-}).get("movie/:type", async (ctx) => {
-  let connect = await swichReactDB(ctx.url.split('/')[1])
-  let query = {count: +ctx.query.count , start: +ctx.query.start}
-  let type = ctx.params.type
-  let res = {}
-  if(type === "search_result"){
-    res = await movieRegexFind(ctx.query.search,query)
-  }else{
-    res = await movieTypeFind(type,query)
-  }
-  // let url = ctx.url.split('/')[2] + '/' + ctx.url.split('/')[3]
-  // let data = await moivesAxios(url, "get")
-  // console.log("movie/in_theaters", data)
-  // let res = await movieInsert(data.subjects)
-  ctx.body = res
-})
+  .post("add", async (ctx) => {
+    console.log("uniapp add", JSON.stringify(ctx.request.body, null, 4));
+    ctx.body = "已加入";
+  });
 
-vueShop.get('shop/list', async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let list = await shopsFindAll()
-  let data = {
-    code: 1,
-    list
-  }
-  ctx.body = data
-}).get("shop/info", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let id = ctx.query.id
-  let info = await shopsFindOne(id)
-  // let info = {title:"华为手机",sell_price:2000,market_price:2500,stock_quantity:200,add_time:1590033781425,goods_no:"1520",
-  // lunbotu:["http://localhost:3000/static/img/vue.png","http://localhost:3000/static/img/koa.jpg"]}
-  let data = {
-    code: 1,
-    info
-  }
-  ctx.body = data
-}).get("shop/add", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let res = await shopInsert()
-  if (res) {
-    ctx.body = "shop success add"
-  } else {
-    ctx.body = "shop fail add"
-  }
-}).get("car/shop-list", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let list = await carFindShopAll()
-  console.log("car/shop-list", list)
-  let data = {
-    code: 1,
-    list
-  }
-  ctx.body = data
-}).get("car/list", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let list = await carFindAll()
-  console.log("car/list", list)
-  let data = {
-    code: 1,
-    list
-  }
-  ctx.body = data
-}).post("car/update", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let body = ctx.request.body
-  let good_id = body.good_id
-  delete body.good_id
-  let res = await carUpdate(good_id, body)
-  console.log("car/update", good_id, body)
-  let data = {
-    code: 0
-  }
-  if (res) { data.code = 1 }
-  ctx.body = data
-}).post("car/delete", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let good_id = ctx.request.body.good_id
-  console.log("car/delete", good_id)
-  let res = await carDelete(good_id)
-  let data = {
-    code: 0
-  }
-  if (res) { data.code = 1 }
-  ctx.body = data
-}).post("car/add", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let body = ctx.request.body
-  console.log("car/add", body)
-  let res = await carInsertOne(body)
-  let data = {
-    code: 0
-  }
-  if (res) { data.code = 1 }
-  ctx.body = data
-}).get("car/add", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let res = await carInsert()
-  if (res) {
-    ctx.body = "car success add"
-  } else {
-    ctx.body = "car fail add"
-  }
-}).get('img/lunbo', async (ctx) => {
-  let data = {
-    code: 1,
-    list: ["http://106.52.147.170:3000/static/img/scenery.jpg",
-      "http://106.52.147.170:3000/static/img/shopcar.gif",
-      "http://106.52.147.170:3000/static/img/news.jpg"]
-  }
-  ctx.body = data
-}).get("news/list", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let list = await newsFindAll()
-  let data = {
-    code: 1,
-    list
-  }
-  ctx.body = data
-}).get("news/info", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let id = ctx.query.id
-  let infoArr = await newsFindOne(id)
-  let data = {
-    code: 1,
-    info: infoArr[0]
-  }
-  ctx.body = data
-}).get("comment/list", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let id = ctx.query.id
-  let list = await commentFindAll(id)
-  let data = {
-    code: 1,
-    list
-  }
-  ctx.body = data
-}).post("comment/add", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let id = ctx.query.id
-  let body = ctx.request.body
-  let res = await commentInsert(id, body)
-  let data = {
-    code: 0
-  }
-  if (res) {
-    data.code = 1
-  }
-  ctx.body = data
-}).get("article/add", async (ctx) => {
-  let connect = await swichVueDB(ctx.url.split('/')[1])
-  let res = await articleInsert()
-  if (res) {
-    ctx.body = 'success add'
-  } else {
-    ctx.body = 'fail add'
-  }
-})
+trq.get("copyClickTask", async (ctx) => {
+  console.log('有请求')
+  ctx.body = {
+    code: 200,
+    data: {
+      planParams: [
+        {
+          id: 3426,
+          click: 10,
+          incrementType: 7,
+          keyword: "华为",
+          executeType: 5,
+          startDate: "2020-10-23",
+          endDate: "2020-10-23",
+          hourParams: [
+            {
+              hour: "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,2,2,1,1,0",
+              date: "2020-10-23",
+            },
+          ],
+          clickTotal: 10,
+        },
+      ],
+      goodsTitle:
+        "【限时享6期免息】Huawei/华为P40 Pro+ 5G 徕卡五摄 100倍变焦陶瓷工艺p40pro+ 5g华为手机华为官方旗舰店",
+      shopId: "150920153",
+      shopName: "华为官方旗舰店",
+      skuUrl: "https://detail.tmall.com/item.htm?id=617308828648",
+      wtUrl: "123",
+      stayType: 2,
+      compare: 0,
+      pvUv: 1,
+      visitOther: 0,
+      visitComment: 1,
+      pttqzType: 6,
+      maleRatio: 30,
+    },
+    msg: null
+  };
+});
+router.use("/react/", reactMovie.routes(), reactMovie.allowedMethods());
+router.use("/uniapp/", uniapp.routes(), uniapp.allowedMethods());
+router.use("/trq/", trq.routes(), trq.allowedMethods());
 
-router.use('/vue/', vueShop.routes(), vueShop.allowedMethods())
-router.use('/react/', reactMovie.routes(), reactMovie.allowedMethods())
-
-module.exports = router
+module.exports = router;
